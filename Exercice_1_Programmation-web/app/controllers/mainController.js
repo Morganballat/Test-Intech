0e0dbf530eb9875e4325a2585936201a1fd3bf2f
Morgan <morgan.ballat@euro4x4parts.com>
2023-04-03 16:53:06 +0200

    gone to far on splitting the app files according to the the statement of the exercise

--- Exercice_1_Programmation-web/app/controllers/mainController.js
+++ Exercice_1_Programmation-web/app/controllers/mainController.js
@@ -1,6 +1,11 @@
 const mainController = {
+
   home: (req, res) => {
       res.render('accueil');
+  },
+
+  form: (req, res) => {
+    res.render('formulaire_creation');
   }
 }
 
--- Exercice_1_Programmation-web/app/router.js
+++ Exercice_1_Programmation-web/app/router.js
@@ -6,5 +6,6 @@ const router = Router();
 const mainController = require('./controllers/mainController');
 
 router.get('/', mainController.home);
+router.get('/create-question-form', mainController.form);
 
 module.exports = router;
